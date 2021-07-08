a869b9425a90bae80993192463de07e9a97529e8
mouadh473 <mouadhfaress@gmail.com>
2021-07-08 17:24:31 +0200

    showing  booking details

---
+++ resources/views/booking/show.blade.php
@@ -0,0 +1,14 @@
+<h1>{{$booking->id}}</h1>
+<table>
+    <thead>
+        <tr>
+            <td>Booking Date</td>
+            <td>Booking Time</td>
+            <td>Number of seats</td>
+            <td>Operation</td>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table>
