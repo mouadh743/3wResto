8141a0897229c0b3368c9e539850b72cec156a68
mouadh473 <mouadhfaress@gmail.com>
2021-07-08 17:35:07 +0200

    html show.nooking

--- resources/views/booking/show.blade.php
+++ resources/views/booking/show.blade.php
@@ -1,14 +1,26 @@
-<h1>{{$booking->id}}</h1>
-<table>
-    <thead>
-        <tr>
-            <td>Booking Date</td>
-            <td>Booking Time</td>
-            <td>Number of seats</td>
-            <td>Operation</td>
-        </tr>
-    </thead>
-    <tbody>
-        
-    </tbody>
-</table>
+@extends('layouts.app')
+
+@section('content')
+    <table class="table table-hover">
+        <thead>
+            <tr>
+              <th scope="col">Booking date</th>
+              <th scope="col">Booking time</th>
+              <th scope="col">Number of seats</th>
+              <th scope="col">Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> {{ $booking->booking_date->format('d/m/Y') }}</td>
+              <td> {{ date('H:i', strtotime($booking->booking_time)) }}</td>
+              <td> {{ $booking->seats_nbr }}</td>
+              <td>
+                  <a href="" class="btn btn-outline-warning">Edit</a>
+                  <a href="" class="btn btn-outline-danger">Delete</a>
+
+              </td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
